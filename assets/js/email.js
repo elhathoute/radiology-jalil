2255495184d24e3a89b0394d2d1d239cdd8745d5
EZ-ZYN Noureddine <noureddinezzyn@gmail.com>
2025-07-07 17:16:56 +0100

    changes

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,42 +1,31 @@
-function Email() {
-    // Affiche "Chargement..."
-    document.querySelector('.loading').style.display = 'block';
-    document.querySelector('.error-message').style.display = 'none';
-    document.querySelector('.sent-message').style.display = 'none';
-  
-    let emailsParms = {
-      subject: document.getElementById("subject").value,
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const service_ID = "service_m4xn97r";
-    const temp_ID = "template_hq0x3zt";
-  
-    emailjs
-      .send(service_ID, temp_ID, emailsParms)
-      .then((res) => {
-        console.log(res);
-  
-        // Cache "Chargement..." et affiche "Sent"
-        document.querySelector('.loading').style.display = 'none';
-        document.querySelector('.sent-message').style.display = 'block';
-  
-        // Vide le formulaire
-        document.getElementById("subject").value = "";
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-      })
-      .catch((err) => {
-        console.log(err);
-  
-        // Cache "Chargement..." et affiche l'erreur
-        document.querySelector('.loading').style.display = 'none';
-        const errorBox = document.querySelector('.error-message');
-        errorBox.innerText = "Erreur lors de l'envoi : " + err;
-        errorBox.style.display = 'block';
-      });
-  }
-  
+function Email(event) {
+  event.preventDefault(); // Prevent the form from submitting the traditional way
+
+  document.querySelector('.loading').style.display = 'block';
+  document.querySelector('.error-message').style.display = 'none';
+  document.querySelector('.sent-message').style.display = 'none';
+
+  const service_ID = "service_m4xn97r";
+  const temp_ID = "template_hq0x3zt";
+
+  emailjs.sendForm(service_ID, temp_ID, '#contactForm')
+    .then((res) => {
+      console.log(res);
+      document.querySelector('.loading').style.display = 'none';
+      document.querySelector('.sent-message').style.display = 'block';
+
+      document.getElementById("subject").value = "";
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+    })
+    .catch((err) => {
+      console.log(err);
+      document.querySelector('.loading').style.display = 'none';
+      const errorBox = document.querySelector('.error-message');
+      errorBox.innerText = "Erreur lors de l'envoi : " + err.text;
+      errorBox.style.display = 'block';
+    });
+
+  // return false; // Not necessary with event.preventDefault()
+}
